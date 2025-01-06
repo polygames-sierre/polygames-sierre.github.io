2de8089aa56674f36d37fdcbaa4e0c754ac4b0cb
Fabrice Theytaz <2198753+fabricetheytaz@users.noreply.github.com>
2025-01-06 17:07:51 +0100

    Rien

--- scripts/polygames.mjs
+++ scripts/polygames.mjs
@@ -1,6 +1,6 @@
-//@ts-ignore
-import nunjucks from "https://cdn.jsdelivr.net/npm/nunjucks@3.2.4/+esm";
-//import * as nunjucks from "nunjucks";
+//import nunjucks from "https://cdn.jsdelivr.net/npm/nunjucks@3.2.4/+esm";
+import * as nunjucks from "nunjucks";
+//nunjucks.precompile()
 async function render(template, model) {
     return new Promise((resolve, reject) => {
         try {
@@ -16,7 +16,7 @@ async function init() {
     let json = await response.json();
     let events = json.agenda.events;
     let event = events[0];
-    render("/temp/event.html", event).then(html => {
+    render("/temp/templates/event.html", event).then(html => {
         document.body.insertAdjacentHTML("beforeend", html);
     });
 }
--- scripts/polygames.mts
+++ scripts/polygames.mts
@@ -1,7 +1,8 @@
 
-//@ts-ignore
-import nunjucks from "https://cdn.jsdelivr.net/npm/nunjucks@3.2.4/+esm";
-//import * as nunjucks from "nunjucks";
+//import nunjucks from "https://cdn.jsdelivr.net/npm/nunjucks@3.2.4/+esm";
+import * as nunjucks from "nunjucks";
+
+//nunjucks.precompile()
 
 async function render(template:string, model:any):Promise<string>
 	{
@@ -28,7 +29,7 @@ async function init()
 
 	let event = events[0];
 
-	render("/temp/event.html", event).then(html =>
+	render("/temp/templates/event.html", event).then(html =>
 		{
 		document.body.insertAdjacentHTML("beforeend", html);
 		});
